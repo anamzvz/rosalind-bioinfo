ddfd94761c1719daddc82d64dd3a250a1a8aecc9
anamzvz <anamarvaz2002@gmail.com>
2025-07-08 13:42:36 +0200

    rosalind3

---
+++ rosalind2.py
@@ -0,0 +1,4 @@
+a = 866
+b = 980
+hipotenusa_cuadrada = (a**2 + b**2)
+print(hipotenusa_cuadrada)
--- rosalind3.py
+++ rosalind3.py
@@ -1,6 +1,6 @@
-s = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain"
-a = 22
-b = 27
-c = 97
-d = 102
+s = "8xGemrGsfS3vf71B8Qh1PZLZHTbSxygyzQujLsJxv6vkynU1aUca3ezpQezV6YHPcIYY7EA6MustelaRMKap49nawt9XvQEsjQrVtneSSQ8w6hoqAxdSrJpQHJ4v3LY05PzRDf0umzm0hhcyanogenysFE288BLNFOzz7uagJ8Cxjl0lRSueLKhb0OLCm"
+a = 72
+b = 78
+c = 142
+d = 151
 print(s[a:b+1], "", s[c:d+1])
